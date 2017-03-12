2d5ac8216e3a253e6bf592061b746c9e812678fe
alexkgrimes <alexkgrimes@gmail.com>
2017-03-11 23:40:38 -0600

    new look
    basics

---
+++ alexgrimes.js
@@ -0,0 +1,30 @@
+
+
+var granimInstance = new Granim({
+    element: '#canvas-basic',
+    name: 'basic-gradient',
+    direction: 'left-right',
+    opacity: [1, 1],
+    isPausedWhenNotInView: true,
+    states : {
+        "default-state": {
+            gradients: [
+                ['#AA076B', '#61045F'],
+                ['#02AAB0', '#00CDAC'],
+                ['#DA22FF', '#9733EE']
+            ],
+            transitionSpeed:5000,
+            loop: true
+        }
+    },
+    onStart: function() {
+      console.log('Granim: onStart');
+    },
+    onGradientChange: function(colorDetails) {
+      console.log('Granim:onGradientChange, details: ');
+      console.log(colorDetails);
+    },
+    onEnd: function() {
+      console.log('Granim: onEnd');
+    }
+});
